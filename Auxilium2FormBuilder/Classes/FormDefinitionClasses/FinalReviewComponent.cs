58f1b6ae2e271374f0cb1c7c65e54885bd921d0d
Cerys <cerysamberlewis@gmail.com>
2025-01-15 23:17:04 +0000

    can so almost recreate the existing formdef files

--- Auxilium2FormBuilder/Classes/FormDefinitionClasses/FinalReviewComponent.cs
+++ Auxilium2FormBuilder/Classes/FormDefinitionClasses/FinalReviewComponent.cs
@@ -12,17 +12,24 @@ namespace Auxilium2FormBuilder.Classes.FormDefinitionClasses
         public required string Type { get; set; }
 
 
-        public required string Value { get; set; }
-        public required string JumpToPage { get; set; }
-        public required string IfStatement { get; set; }
+        public required string? Value { get; set; }
+        public required string? JumpToPage { get; set; }
+        public required string? IfStatement { get; set; }
+
+        public required List<Tuple<string, string>>? Dictionary { get; set; }
 
         public static FinalReviewComponent FromJSON(JsonNode jsonNode)
         {
             string type = jsonNode["type"]?.GetValue<string>() ?? throw new InvalidOperationException("Missing 'type' in JSON.");
 
-            string value = jsonNode["value"]?.GetValue<string>() ?? null;
-            string jumpToPage = jsonNode["jump_to_page"]?.GetValue<string>() ?? null;
-            string ifStatement = jsonNode["if"]?.GetValue<string>() ?? null;
+            string? value = jsonNode["value"]?.GetValue<string>() ?? null;
+            string? jumpToPage = jsonNode["jump_to_page"]?.GetValue<string>() ?? null;
+            string? ifStatement = jsonNode["if"]?.GetValue<string>() ?? null;
+            List<Tuple<string, string>>? dictionary = jsonNode["dictionary"] is JsonObject dictionaryNode
+                ? dictionaryNode
+                    .Select(kvp => new Tuple<string, string>(kvp.Key, kvp.Value!.GetValue<string>()))
+                    .ToList()
+                : null;
 
             FinalReviewComponent builder = new()
             {
@@ -30,7 +37,8 @@ namespace Auxilium2FormBuilder.Classes.FormDefinitionClasses
 
                 Value = value,
                 JumpToPage = jumpToPage,
-                IfStatement = ifStatement
+                IfStatement = ifStatement,
+                Dictionary = dictionary,
             };
 
             return builder;
@@ -46,6 +54,15 @@ namespace Auxilium2FormBuilder.Classes.FormDefinitionClasses
             if (this.Value != null) jsonObject["value"] = this.Value;
             if (this.JumpToPage != null) jsonObject["jump_to_page"] = this.JumpToPage;
             if (this.IfStatement != null) jsonObject["if"] = this.IfStatement;
+            if (this.Dictionary != null)
+            {
+                var dictionaryNode = new JsonObject();
+                foreach (var (key, value) in this.Dictionary)
+                {
+                    dictionaryNode[key] = value;
+                }
+                jsonObject["dictionary"] = dictionaryNode;
+            }
 
             return jsonObject;
         }
